bd49b9bd6e20deba16fe07eb3065ceb4eb24579c
matt <mstar4enkoo@gmail.com>
2026-09-09 19:59:40 +0500

    Task1

---
+++ Tyuiu.StarchenkoMV.Sprint0.Task1.V0/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Tyuiu.StarchenkoMV.Sprint0.Task1.V0;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        string name = "Матвей";
+        int age = 18;
+        Console.WriteLine("Здравствуйте, меня зовут " + name + ", мне " + age + " лет!");
+        Console.ReadKey();
+    }
+}
