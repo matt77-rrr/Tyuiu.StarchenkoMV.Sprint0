4d0f7b708e4953a481e9f26e0889c04073a7c4c3
matt <mstar4enkoo@gmail.com>
2026-09-12 16:27:12 +0500

    Task2

---
+++ Tyuiu.StarchenkoMV.Sprint0.Task2.V0/Program.cs
@@ -0,0 +1,13 @@
+﻿using Tyuiu.StarchenkoMV.Sprint0.Task2.V0.Lib;
+namespace Tyuiu.StarchenkoMV.Sprint0.Task2.V0;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine(DataService.GetMessage("Матвей"));
+        Console.ReadKey();
+        Console.ReadKey();
+        
+    }
+}
