d0c74b15d5e94facc2236ec9eb5288e13f6d13a9
matt <mstar4enkoo@gmail.com>
2026-09-12 16:26:42 +0500

    Task5

---
+++ Tyuiu.StarchenkoMV.Sprint0.Task5.V0.Test/DataServiceTest.cs
@@ -0,0 +1,34 @@
+using Tyuiu.StarchenkoMV.Sprint0.Task5.V0.Lib;
+namespace Tyuiu.StarchenkoMV.Sprint0.Task5.V0.Test;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.AreEqual(10,DataService.Addition(5,5));
+    }
+
+    [Test]
+    public void Test2()
+    {
+        Assert.AreEqual(5,DataService.Subtraction(10,5));
+    }
+
+    [Test]
+    public void Test3()
+    {
+        Assert.AreEqual(50,DataService.Multiplication(5,10));
+    }
+
+    [Test]
+    public void Test4()
+    {
+        Assert.AreEqual(3,DataService.Division(9,3));
+    }
+}
